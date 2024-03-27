a49417dde2209d8b19911cd14b10c4489449416a
MarbledNull <cyandoezstuff@gmail.com>
2024-03-27 00:35:08 -0500

    initial commit

--- src/main/java/net/marblednull/marbledsarsenal/init/ModItemProperties.java
+++ src/main/java/net/marblednull/marbledsarsenal/init/ModItemProperties.java
@@ -9,11 +9,9 @@ public class ModItemProperties {
         //makeShield(ModItems.BALLISTIC_SHIELD.get());
         //makeShield(ModItems.RIOT_SHIELD.get());
     }
-
     private static void makeShield(Item item) {
         ItemProperties.register(item, new ResourceLocation("blocking"), (p_174590_, p_174591_, p_174592_, p_174593_) -> {
             return p_174592_ != null && p_174592_.isUsingItem() && p_174592_.getUseItem() == p_174590_ ? 1.0F : 0.0F;
         });
-
     }
 }
