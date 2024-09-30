09d94ef907d31349875e433f601cbf3210ed2506
MarbledNull <cyandoezstuff@gmail.com>
2024-09-30 15:41:27 -0500

    added armor materials

--- src/main/java/net/marblednull/marbledsarsenal/materials/MaArmorMaterials.java
+++ src/main/java/net/marblednull/marbledsarsenal/materials/MaArmorMaterials.java
@@ -14,7 +14,7 @@ import java.util.function.Supplier;
 
 public enum MaArmorMaterials implements ArmorMaterial {
 
-    HAT("hat", 10, new int[]{2, 6, 5, 2}, 15,
+    HAT("hat", 10, new int[]{1, 6, 5, 2}, 15,
             SoundEvents.ARMOR_EQUIP_LEATHER, 0f, 0f, () ->
             Ingredient.of(new ItemLike[]{Items.LEATHER})),
 
@@ -23,43 +23,34 @@ public enum MaArmorMaterials implements ArmorMaterial {
             Ingredient.of(ModItems.STEEL_INGOT.get())),
 
     PLATE_CARRIER_LIGHT("plate_carrier_light", 20, new int[]{2, 6, 5, 2}, 15,
-            SoundEvents.ARMOR_EQUIP_IRON, 0f, 0f, () ->
+            SoundEvents.ARMOR_EQUIP_IRON, 0.5f, 0f, () ->
             Ingredient.of(ModItems.STEEL_INGOT.get())),
 
-    PLATE_CARRIER_HEAVY("plate_carrier_heavy", 20, new int[]{2, 6, 5, 2}, 15,
-            SoundEvents.ARMOR_EQUIP_IRON, 1f, 0f, () ->
+    PLATE_CARRIER_HEAVY("plate_carrier_heavy", 20, new int[]{2, 8, 5, 2}, 15,
+            SoundEvents.ARMOR_EQUIP_IRON, 1.5f, 0.1f, () ->
             Ingredient.of(ModItems.STEEL_INGOT.get())),
 
-    GAS_MASK("gas_mask", 15, new int[]{2, 6, 5, 2}, 15,
+    GAS_MASK("gas_mask", 15, new int[]{1, 6, 5, 2}, 15,
                 SoundEvents.ARMOR_EQUIP_IRON, 0f, 0f, () ->
             Ingredient.of(ModItems.STEEL_INGOT.get())),
 
-    HAZMAT("hazmat", 13, new int[]{2, 6, 5, 2}, 15,
+    HAZMAT("hazmat", 13, new int[]{1, 3, 2, 1}, 15,
             SoundEvents.ARMOR_EQUIP_LEATHER, 0f, 0f, () ->
             Ingredient.of(new ItemLike[]{Items.LEATHER})),
-    GHILLIE("ghillie", 10, new int[]{2, 6, 5, 2}, 15,
+    GHILLIE("ghillie", 10, new int[]{1, 3, 2, 1}, 15,
             SoundEvents.ARMOR_EQUIP_LEATHER, 0f, 0f, () ->
             Ingredient.of(new ItemLike[]{Items.LEATHER})),
 
-    POLICEFORCE("policeforce", 25, new int[]{2, 6, 5, 2}, 15,
-            SoundEvents.ARMOR_EQUIP_IRON, 2f, 0f, () ->
+    POLICEFORCE("policeforce", 25, new int[]{3, 7, 5, 3}, 15,
+            SoundEvents.ARMOR_EQUIP_IRON, 2f, 0.1f, () ->
             Ingredient.of(ModItems.STEEL_INGOT.get())),
 
-    MILITARY("military", 33, new int[]{2, 6, 5, 2}, 15,
-            SoundEvents.ARMOR_EQUIP_IRON, 2f, 0f, () ->
+    MILITARY("military", 33, new int[]{3, 8, 6, 3}, 15,
+            SoundEvents.ARMOR_EQUIP_IRON, 2f, 0.1f, () ->
             Ingredient.of(ModItems.STEEL_INGOT.get())),
 
-    JUGGERNAUT("juggernaut", 37, new int[]{2, 6, 5, 2}, 15,
+    JUGGERNAUT("juggernaut", 37, new int[]{3, 9, 7, 3}, 15,
             SoundEvents.ARMOR_EQUIP_NETHERITE, 3f, 0.2f, () ->
-            Ingredient.of(ModItems.TITANIUM_INGOT.get())),
-
-
-    STEEL("steel", 33, new int[]{2, 6, 5, 2}, 15,
-            SoundEvents.ARMOR_EQUIP_NETHERITE, 0f, 0f, () ->
-            Ingredient.of(ModItems.STEEL_INGOT.get())),
-
-    TITANIUM("titanium", 15, new int[]{3, 6, 8, 3}, 10,
-            SoundEvents.ARMOR_EQUIP_NETHERITE, 3f, 0.1f, () ->
             Ingredient.of(ModItems.TITANIUM_INGOT.get()));
 
     private static final int[] BASE_DURABILITY = {13, 15, 16, 11};
